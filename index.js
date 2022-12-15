994aad9210d2fab4936ae98c1c83a57813f25bd4
Jasmine Cheung <jasminesyc2000@gmail.com>
2022-12-15 12:14:18 -0500

    intermediate lab completed

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 // console.log("hello world");
 const message = document.querySelector("#message");
 
-function deleteMovie(event) {
-  message.textContent = "Movie deleted!";
-  event.target.parentNode.remove();
-  console.log("deleted");
+function revealMessage() {
+  message.classList.remove("hide");
+  setTimeout(() => message.classList.add("hide"), 1000);
 }
+
+function deleteMovie(event) {
+  message.textContent = `${event.target.previousElementSibling.textContent} deleted!`;
+  event.target.parentNode.remove();
+  revealMessage();
+}
+
 function crossOffMovie(event) {
   event.target.classList.toggle("checked");
   if (event.target.classList.contains("checked")) {
-    message.textContent = "Movie watched!";
+    message.textContent = `${event.target.textContent} watched!`;
   } else {
-    message.textContent = "Movie added back!";
+    message.textContent = `${event.target.textContent} added back!`;
   }
 }
 function addMovie(event) {
